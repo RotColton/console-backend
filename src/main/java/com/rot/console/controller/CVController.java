d3a09f83cdfc433d1bf6ede955d7a85c1415d284
RotColton <maromidomingo@gmail.com>
2024-11-04 01:03:44 +0100

    Initial commit for reactive back-end project

---
+++ src/main/java/com/rot/console/controller/CVController.java
@@ -0,0 +1,25 @@
+package com.rot.console.controller;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class CVController {
+
+    @GetMapping("/download-cv")
+    public Mono<ResponseEntity<Resource>> downloadCV() {
+        return Mono.fromSupplier(() -> {
+            Resource resource = new ClassPathResource("files/resume.pdf");
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"resume.pdf\"")
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(resource);
+        });
+    }
+}
