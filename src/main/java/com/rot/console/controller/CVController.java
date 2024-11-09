7ce8351fac6a1a2d575f6012d6d388b05517f01d
RotColton <maromidomingo@gmail.com>
2024-11-09 14:34:25 +0100

    docker image and CrossOrigin

--- src/main/java/com/rot/console/controller/CVController.java
+++ src/main/java/com/rot/console/controller/CVController.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -12,6 +13,7 @@ import reactor.core.publisher.Mono;
 @RestController
 public class CVController {
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/download-cv")
     public Mono<ResponseEntity<Resource>> downloadCV() {
         return Mono.fromSupplier(() -> {
